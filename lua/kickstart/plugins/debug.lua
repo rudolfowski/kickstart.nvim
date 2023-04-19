79e31276f6268ba6e72148e8c381dca339054f25
rudolfowski <rudolfowski@gmail.com>
2023-04-20 01:58:00 +0200

    added sessions filetree and debug for typescript and javascript

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -5,6 +5,7 @@
 -- Primarily focused on configuring the debugger for Go, but can
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
+local DEBUGGER_PATH = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug"
 
 return {
   -- NOTE: Yes, you can install new plugins here!
@@ -20,6 +21,13 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+
+    -- Javascript
+    { "mxsdev/nvim-dap-vscode-js" },
+    {
+      "microsoft/vscode-js-debug",
+      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+    },
   },
   config = function()
     local dap = require 'dap'
@@ -43,14 +51,37 @@ return {
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<F5>', dap.continue)
+    vim.keymap.set('n', '<F1>', dap.step_over)
+    vim.keymap.set('n', '<F2>', dap.step_into)
+    vim.keymap.set('n', '<F3>', dap.step_back)
+    vim.keymap.set('n', '<F4>', dap.step_out)
+    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
+    vim.keymap.set('n', '<Leader>dr', function()
+      dap.repl.toggle({
+        height = 10,
+      })
+    end)
+    vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+      require('dap.ui.widgets').hover()
+    end)
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+      require('dap.ui.widgets').preview()
+    end)
+    vim.keymap.set('n', '<Leader>df', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.frames)
+    end)
+    vim.keymap.set('n', '<Leader>ds', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.scopes)
+    end)
+
+    vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
@@ -72,6 +103,30 @@ return {
           disconnect = '⏏',
         },
       },
+      layouts = {
+        {
+          -- You can change the order of elements in the sidebar
+          elements = {
+            -- Provide IDs as strings or tables with "id" and "size" keys
+            {
+              id = "scopes",
+              size = 0.25, -- Can be float or integer > 1
+            },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks",      size = 0.25 },
+            { id = "watches",     size = 0.25 },
+          },
+          size = 40,
+          position = "left", -- Can be "left" or "right"
+        },
+        --{
+        --  elements = {
+        --    "repl",
+        --  },
+        --  size = 10,
+        --  position = "bottom", -- Can be "bottom" or "top"
+        --},
+      },
     }
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
@@ -83,5 +138,46 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup()
+
+
+    require("dap-vscode-js").setup({
+      --node_path = "node",
+      debugger_path = DEBUGGER_PATH,
+      -- debugger_cmd = { "js-debug-adapter" },
+      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+    })
+
+    for _, language in ipairs { "typescript", "javascript" } do
+      require("dap").configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach Node",
+          --processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+          port = 9229,
+        },
+        {
+          type = "pwa-chrome",
+          name = "Launch Chrome",
+          request = "launch",
+          sourceMaps = true,
+          url = "http://localhost:4200",
+          webRoot = "${workspaceFolder}",
+          runtimeArgs = { "--profile-directory=Default" },
+          userDataDir = false,
+          restart = true
+        },
+        {
+          type = "pwa-chrome",
+          name = "Attach Chrome",
+          request = "attach",
+          sourceMaps = true,
+          url = "http://localhost:4200",
+          port = 9222,
+          webRoot = "${workspaceFolder}",
+        },
+      }
+    end
   end,
 }
